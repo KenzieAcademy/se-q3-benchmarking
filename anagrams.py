d2c381146254dd2cdaf9f1daeaddecd893645cc9
morthos <pathshan61@gmail.com>
2019-03-21 15:06:26 -0400

    Update anagrams.py

--- anagrams.py
+++ anagrams.py
@@ -8,7 +8,7 @@
     for an arbitrary list of strings.
 
 """
-__author__ = "???"
+__author__ = "PK013"
 
 import sys
 
@@ -39,20 +39,24 @@ def find_anagrams(words):
         {'dgo': ['dog'], 'act': ['cat', 'act']}
 
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
-    return anagrams
+    anagrams = {}
+    for word in words:
+        w = alphabetize(word)
+        if w in anagrams:
+            anagrams[w].append(word)
+        else:
+            anagrams[w] = [word]
+        return anagrams
+
 
 
 if __name__ == "__main__":
     # run find anagrams of first argument
     if len(sys.argv) < 2:
-        print "Please specify a word file!"
+        print ("Please specify a word file!")
         sys.exit(1)
     else:
         with open(sys.argv[1], 'r') as handle:
             words = handle.read().split()
-            print find_anagrams(words)
+            words.pop(0)
+            print (find_anagrams(words))
